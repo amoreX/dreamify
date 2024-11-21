0a0cb60ffd3bc808d51d66a079529ffb6edc84a2
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-11-21 16:23:50 +0530

    database-connection-done

--- src/app/pages/homepage/page.js
+++ src/app/pages/homepage/page.js
@@ -1,7 +1,17 @@
 "use client"
+import axios from "axios";
 import { useSession } from "next-auth/react"
+import { useState,useEffect } from "react";
 export default function Homepage (){
     const {data: session}=useSession();
+
+    useEffect(()=>{
+        const getData=async()=>{
+            const diddy=await axios.get("/api/connect");
+        };
+        getData();
+    },[]);
+
     return(
         <div>{session?.user?.name}</div>
     )
